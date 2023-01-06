9f0c23f6cb152475ca74a174562f27833212b0aa
Stolbin <stolbin.n@gmail.com>
2023-01-06 13:37:35 +0200

    Update loadMoreBtn.js

--- src/js/loadMoreBtn.js
+++ src/js/loadMoreBtn.js
@@ -9,20 +9,17 @@ export default class LoadMoreButton {
     const refs = {};
     refs.button = document.querySelector(selector);
     refs.label = refs.button.querySelector('.label');
-    refs.spinner = refs.button.querySelector('.spinner');
     return refs;
   }
 
   enable() {
     this.refs.button.disabled = false;
     this.refs.label.textContent = 'Load more';
-    this.refs.spinner.classList.add('is-hidden');
   }
 
   disable() {
     this.refs.button.disabled = true;
     this.refs.label.textContent = 'Loading...';
-    this.refs.spinner.classList.remove('is-hidden');
   }
 
   show() {
