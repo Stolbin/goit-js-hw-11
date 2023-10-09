4c6d8d0983ed4199536b404f16f7578a9352d70b
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-10-09 14:46:49 +0300

    Update loadMoreBtn.js

--- src/js/loadMoreBtn.js
+++ src/js/loadMoreBtn.js
@@ -9,17 +9,20 @@ export default class LoadMoreButton {
     const refs = {};
     refs.button = document.querySelector(selector);
     refs.label = refs.button.querySelector(".label");
+    refs.spinner = refs.button.querySelector(".spinner");
     return refs;
   }
 
   enable() {
     this.refs.button.disabled = false;
     this.refs.label.textContent = "Load more";
+    this.refs.button.classList.add("is-hidden");
   }
 
   disable() {
     this.refs.button.disabled = true;
     this.refs.label.textContent = "Loading...";
+    this.refs.spinner.classList.remove("is-hidden");
   }
 
   show() {
